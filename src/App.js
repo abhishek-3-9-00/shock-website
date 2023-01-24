fe2c7f98c8753487ed41eb074130587dc5ddc633
Abhishek Verma <107533283+abhishek-3-9-00@users.noreply.github.com>
2023-01-24 16:38:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header/nav/nav/Header';
+import Feature from './components/feature/Feature';
+import Newsletter from './components/newsletter-section/Newsletter';
+import Footer from './components/footer/Footer';
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Feature/>
+    <Newsletter/>
+    <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
